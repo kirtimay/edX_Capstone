233fcbf3a3a4d7d1d3f011efca9f2a8bde5b79b2
Kirtimay Pendse <kirtimay@gmail.com>
2020-06-12 19:48:12 +0530

    updating R script

--- movielens/movieLens.R
+++ movielens/movieLens.R
@@ -3,9 +3,9 @@
 ## HarvardX: PH125.9x Data Science: Capstone
 ## https://github.com/kirtimay/edX_Capstone/
 
-################################
-# 1. Create edx set, validation set
-################################
+#####################################################
+# 1. Create edx set, validation set (the following code was given by the edX staff)
+#####################################################
 
 # Note: this process could take a couple of minutes
 
@@ -32,7 +32,7 @@ movies <- as.data.frame(movies) %>% mutate(movieId = as.numeric(levels(movieId))
 movielens <- left_join(ratings, movies, by = "movieId")
 
 # Validation set will be 10% of MovieLens data
-set.seed(1, sample.kind="Rounding")
+set.seed(1, sample.kind="Rounding") #use set.seed(1) if using R 3.5 or earlier
 test_index <- createDataPartition(y = movielens$rating, times = 1, p = 0.1, list = FALSE)
 edx <- movielens[-test_index,]
 temp <- movielens[test_index,]
@@ -52,49 +52,82 @@ rm(dl, ratings, movies, test_index, temp, movielens, removed)
 # 2. Exploratory Data Analysis
 ################################
 
-#How many rows and columns are there in the edx dataset?
-dim(edx)
+head(edx) %>% print.data.frame() #to inspect the data briefly, and get a 'feel'
 
-#How many zeros were given as ratings in the edx dataset?
-edx %>% filter(rating == 0) %>% tally()
+sapply(edx, class) #ascertain the class of each variable
 
-#How many threes were given as ratings in the edx dataset?
-edx %>% filter(rating == 3) %>% tally()
+summary(edx) #get summary stats for the individual variables
 
-#How many different movies are in the edx dataset?
-n_distinct(edx$movieId)
+### Inspect the variables in more detail
 
-#How many different users are in the edx dataset?
-n_distinct(edx$userId)
+#Date
 
-#How many movie ratings are in each of the following genres in the edx dataset?
-#Drama
-drama <- edx %>% filter(str_detect(genres,"Drama"))
-paste('Drama has', nrow(drama), 'movies')
-#Comedy
-comedy <- edx %>% filter(str_detect(genres, "Comedy"))
-paste('Comedy has', nrow(comedy), 'movies')
-#Thriller
-thriller <- edx %>% filter(str_detect(genres, "Thriller"))
-paste('Thriller has', nrow(thriller), 'movies')
-#Romance
-romance <- edx %>% filter(str_detect(genres, "Romance"))
-paste('Romance has', nrow(romance), 'movies')
+library(lubridate)
 
-#Which movie has the greatest number of ratings?
-edx %>% group_by(movieId, title) %>%
-  summarize(count = n()) %>%
-  arrange(desc(count))
+if(!require(ggthemes)) 
+  install.packages("ggthemes", repos = "http://cran.us.r-project.org")
+if(!require(scales)) 
+  install.packages("scales", repos = "http://cran.us.r-project.org")
 
-#What are the five most given ratings in order from most to least?
-edx %>% 
-  group_by(rating) %>% 
-  summarize(count = n()) %>% 
-  top_n(5) %>%
-  arrange(desc(count))
+edx %>% mutate(year = year(as_datetime(timestamp, origin="1970-01-01"))) %>%
+  ggplot(aes(x=year)) +
+  geom_histogram(color = "black", bins = 30) + 
+  ggtitle("Distribution of Ratings across Years") +
+  xlab("Year") +
+  ylab("# Ratings") +
+  scale_y_continuous(labels = comma) 
 
-#True or False- half star ratings are less common than whole star ratings
-edx %>% group_by(rating) %>% summarize(count = n()) #T
+#Movies
+
+edx %>% group_by(movieId) %>%
+  summarise(n=n()) %>%
+  ggplot(aes(n)) +
+  geom_histogram(color = "black", bins = 30) +
+  scale_x_log10() + 
+  ggtitle("Distribution of Movies") +
+  xlab("# Ratings") +
+  ylab("# Movies") 
+
+#Users
+
+edx %>% group_by(userId) %>%
+  summarise(n=n()) %>%
+  ggplot(aes(n)) +
+  geom_histogram(color = "black", bins = 30) +
+  scale_x_log10() + 
+  ggtitle("Distribution of Users") +
+  xlab("# Ratings") +
+  ylab("# Users") + 
+  scale_y_continuous(labels = comma) 
+
+#Ratings
+
+edx %>% group_by(rating) %>% 
+  summarise(count=n()) %>%
+  ggplot(aes(x=rating, y=count)) + 
+  geom_line() +
+  geom_point() +
+  scale_y_log10(breaks = trans_breaks("log10", function(x) 10^x),
+                labels = trans_format("log10", math_format(10^.x))) +
+  ggtitle("Distribution of Ratings") + 
+  xlab("Rating") +
+  ylab("Count") 
+
+################################
+# 3. Building the Model
+################################
+
+#Defining the RMSE, our tool for evaluation
+
+RMSE <- function(true_ratings, predicted_ratings){
+  sqrt(mean((true_ratings - predicted_ratings)^2))
+}
+
+
+
+
+
+
 
 
 
